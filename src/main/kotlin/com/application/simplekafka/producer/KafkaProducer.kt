40a388dc7a047d5d8284482e4cdc2a87df6fd4ac
heshAlgo <serverdeveloper@naver.com>
2021-12-18 17:09:27 +0900

    Change kafka producer

--- src/main/kotlin/com/application/simplekafka/producer/KafkaProducer.kt
+++ src/main/kotlin/com/application/simplekafka/producer/KafkaProducer.kt
@@ -1,24 +1,24 @@
 package com.application.simplekafka.producer
 
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
 @Component
 class KafkaProducer(
-    val properties: KafkaProducerProperties
+    val kafkaTemplate: KafkaTemplate<String, String>
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
 
     @Bean
-    fun create() {
-        log.info("======== Start Producer ========")
-        val kafkaProducer = KafkaProducer<String, String>(properties.producerConfigs())
-        val kafkaRecord = ProducerRecord<String, String>("create-spring-topic", "input-value")
-        kafkaProducer.send(kafkaRecord)
-        kafkaProducer.close()
-        log.info("======== Close Producer ========")
+    fun sendTopicMessage() {
+        try {
+            log.info("======== Start Producer ========")
+            kafkaTemplate.send("create-spring-topic", "input-value")
+            log.info("======== Close Producer ========")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
