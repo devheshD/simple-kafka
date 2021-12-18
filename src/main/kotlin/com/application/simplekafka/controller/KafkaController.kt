a88b173dd8b476652492036538c4c34bdf34a820
heshAlgo <serverdeveloper@naver.com>
2021-12-18 22:44:40 +0900

    Delete kafka consumer

--- src/main/kotlin/com/application/simplekafka/controller/KafkaController.kt
+++ src/main/kotlin/com/application/simplekafka/controller/KafkaController.kt
@@ -1,8 +1,6 @@
 package com.application.simplekafka.controller
 
-import com.application.simplekafka.consumer.KafkaConsumer
 import com.application.simplekafka.producer.KafkaProducer
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class KafkaController(
     private val kafkaProducer: KafkaProducer,
-    private val kafkaConsumer: KafkaConsumer,
 ) {
     @PostMapping
     fun createMessage(
